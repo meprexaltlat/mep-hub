4ea59ec2f6eed2c18ff47f0b158a3257aed5b7eb
meprexaltlat <meprex123123@gmail.com>
2025-04-23 00:13:09 +1200

    Create Mep_Hub.lua

---
+++ Mep_Hub.lua
@@ -0,0 +1,96 @@
+debugX = true
+
+-- Play a startup sound
+local Sound = Instance.new("Sound", game:GetService("SoundService"))
+Sound.SoundId = "rbxassetid://9118823102" -- Example startup sound, replace with your own if you want
+Sound.Volume = 1
+Sound:Play()
+
+-- Load Rayfield UI
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "meprex script",
+   Icon = 0,
+   LoadingTitle = "mep script",
+   LoadingSubtitle = "by meprex",
+   Theme = "DarkBlue", -- Changed theme
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+
+   KeySystem = false,
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Hello"}
+   }
+})
+
+-- Main Scripts Tab
+local Tab = Window:CreateTab("Scripts", 4483362458)
+local Section = Tab:CreateSection("Script Execution")
+
+Tab:CreateButton({
+   Name = "Infinite Yield",
+   Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+   end,
+})
+
+Tab:CreateButton({
+   Name = "Rewind Script",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/AKadminlol/ReversebyAK/refs/heads/main/holdCtoReverse"))()
+   end,
+})
+
+Tab:CreateButton({
+   Name = "Face-Bang",
+   Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/8973951821973068216/11374577211057006363/refs/heads/main/antibang'))()
+   end,
+})
+
+Tab:CreateButton({
+   Name = "Trip Script",
+   Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/meprexaltlat/trip-script/refs/heads/main/trip.lua'))()
+   end,
+})
+
+Tab:CreateButton({
+   Name = "Baseplate Script",
+   Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/meprexalt/baseplate/refs/heads/main/baseplate/baseplate.lua'))()
+   end,
+})
+
+-- Misc Tab
+local MiscTab = Window:CreateTab("Misc", 4483362458)
+local MiscSection = MiscTab:CreateSection("Extra Utilities")
+
+MiscTab:CreateButton({
+   Name = "Say Hello In Chat",
+   Callback = function()
+      game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hello from meprex script!", "All")
+   end,
+})
+
+Rayfield:LoadConfiguration()
